75000b9cbc9433dfc64629aef5ddf72299aa988a
Valeriy Bezuglyy <valnocorner@gmail.com>
2019-02-17 13:32:33 +0300

    added project tot support Cartahge

---
+++ Coordinator/Coordinator/Coordinator.h
@@ -0,0 +1,19 @@
+//
+//  Coordinator.h
+//  Coordinator
+//
+//  Created by Valeriy Bezuglyy on 17/02/2019.
+//  Copyright © 2019 Valeriy Bezuglyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Coordinator.
+FOUNDATION_EXPORT double CoordinatorVersionNumber;
+
+//! Project version string for Coordinator.
+FOUNDATION_EXPORT const unsigned char CoordinatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Coordinator/PublicHeader.h>
+
+
